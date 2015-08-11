84066887fb093ed0bb9da98a2914b464213c5c85
Mehmet Beydogan <mbbrain@gmail.com>
2015-08-11 17:28:45 +0300

    updates articles controller:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
@@ -11,8 +12,34 @@ class ArticlesController < ApplicationController
         require(:article).
         permit(:title, :content)
     @article = Article.new(article_params)
-    @article.save
+
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
   end
 
+  def show
+    id = params[:id]
+    @article = Article.find(id)
+  end
 
+  def edit
+    id = params[:id]
+    @article = Article.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @article = Article.find(id)
+    article_params = params.
+        require(:article).
+        permit(:title, :content)
+    if @article.update(article_params)
+      redirect_to article_path(@article.id)
+    else
+      render :edit
+    end
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
+  validates :title, presence: true,
+            length: { minimum: 5 }
 
+  validates :content, length: { minimum: 5 }
 end
